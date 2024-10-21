51a689209d760bd95f0189dc31294e4cbd2fba87
f0ho <3995156363@qq.com>
2024-10-21 10:50:51 +0800

    fix heapSort bug

--- src/main.c
+++ src/main.c
@@ -46,23 +46,24 @@ void main(){
     //堆排序测试
     // struct rusage usage;
     // getrusage(RUSAGE_SELF, &usage);
-    // const int length = 10;
-    // int a[length];
-    // int index = 0;
-    // FILE* f = fopen("../data/1_1.txt","r");
+    // const int length = 100000;
+    // int a[length + 1];//第一个为占位符
+    // a[0] = 1;
+    // int index = 1;
+    // FILE* f = fopen("../data/5_3.txt","r");
     // while(fscanf(f,"%d",&a[index]) == 1){
     //     index++;
     // }
     // fclose(f);
 
     // clock_t start = clock();
-    // int* b = (int*)malloc(sizeof(int) * length);
+    // int* b = (int*)malloc(sizeof(int) * length + 1);
     // b = heapSort(a, length);
     // clock_t end = clock();
     // double timeUsed = ((double)(end - start)) / CLOCKS_PER_SEC;
-    // FILE* file = fopen("../data/O0/heapSort_1_1.txt","w");
+    // FILE* file = fopen("../data/Ofast/heapSort_5_3.txt","w");
     // fprintf(file, "用时:%fs\n占用内存%ldKB\n排序结果为:\n", timeUsed, usage.ru_maxrss);
-    // for(int i = 0; i < length; i++){
+    // for(int i = 1; i < length + 1; i++){
     //     fprintf(file, "%d ",b[i]);
     // }
     // fclose(file);
@@ -93,9 +94,9 @@ void main(){
     // fclose(file);
 
     //对实验数据求平均值
-    // FILE* f1 = fopen("../data/Ofast/fibSort_5_1.txt","r");
-    // FILE* f2 = fopen("../data/Ofast/fibSort_5_2.txt","r");
-    // FILE* f3 = fopen("../data/Ofast/fibSort_5_3.txt","r");
+    // FILE* f1 = fopen("../data/O0/heapSort_1_1.txt","r");
+    // FILE* f2 = fopen("../data/O0/heapSort_1_2.txt","r");
+    // FILE* f3 = fopen("../data/O0/heapSort_1_3.txt","r");
     // int m1,m2,m3,m0;
     // double t1,t2,t3,t0;
     // char buffer[100];
@@ -119,7 +120,7 @@ void main(){
     // sscanf(buffer, "占用内存%dKB", &m3);
     // m0 = (m1 + m2 + m3) / 3;
     // t0 = (t1 + t2 + t3) / 3;
-    // FILE* f = fopen("../data/result/Ofast/fibSort5.txt","w");
+    // FILE* f = fopen("../data/result/O0/heapSort1.txt","w");
     // fprintf(f,"平均用时:%lfs\n占用%dKB内存",t0,m0);
     // fclose(f1);
     // fclose(f2);
